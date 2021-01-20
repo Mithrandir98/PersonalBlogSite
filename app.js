aadd1d2c076b97c5c2c92af118cd941513e39698
Ruthwik H Param <aspire1998@gmail.com>
2021-01-20 16:06:47 +0700

    Final commit

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const bodyParser = require("body-parser");
 const ejs = require("ejs");
+const dash = require('lodash');
 
 const homeStartingContent = "Lacus vel facilisis volutpat est velit egestas dui id ornare. Semper auctor neque vitae tempus quam. Sit amet cursus sit amet dictum sit amet justo. Viverra tellus in hac habitasse. Imperdiet proin fermentum leo vel orci porta. Donec ultrices tincidunt arcu non sodales neque sodales ut. Mattis molestie a iaculis at erat pellentesque adipiscing. Magnis dis parturient montes nascetur ridiculus mus mauris vitae ultricies. Adipiscing elit ut aliquam purus sit amet luctus venenatis lectus. Ultrices vitae auctor eu augue ut lectus arcu bibendum at. Odio euismod lacinia at quis risus sed vulputate odio ut. Cursus mattis molestie a iaculis at erat pellentesque adipiscing.";
 const aboutContent = "Hac habitasse platea dictumst vestibulum rhoncus est pellentesque. Dictumst vestibulum rhoncus est pellentesque elit ullamcorper. Non diam phasellus vestibulum lorem sed. Platea dictumst quisque sagittis purus sit. Egestas sed sed risus pretium quam vulputate dignissim suspendisse. Mauris in aliquam sem fringilla. Semper risus in hendrerit gravida rutrum quisque non tellus orci. Amet massa vitae tortor condimentum lacinia quis vel eros. Enim ut tellus elementum sagittis vitae. Mauris ultrices eros in cursus turpis massa tincidunt dui.";
@@ -33,6 +34,7 @@ app.get("/compose", (req, res) => [
   res.render("compose")
 ]);
 
+
 app.post("/compose", (req, res) => {
   let post = {
     title: req.body.postTitle,
@@ -41,6 +43,22 @@ app.post("/compose", (req, res) => {
   posts.push(post);
   res.redirect("/");
 })
+
+app.get("/posts/:postName", (req, res) => {
+  let requestedTitle = dash.lowerCase(req.params.postName);
+
+  posts.forEach(function(post) {
+    let storedTitle = dash.lowerCase(post.title);
+    if (requestedTitle === storedTitle) {
+      res.render("post", 
+      {
+        title: dash.capitalize(storedTitle), 
+        content: post.content
+      })
+    }
+  });
+})
+
 
 app.listen(3000, function() {
   console.log("Server started on port 3000");
